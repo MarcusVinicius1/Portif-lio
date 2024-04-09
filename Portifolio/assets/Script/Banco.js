d50b625d98c5fd91f6d53a3d5cda784361e9e255
Marcus-Vinicius <91153529+MarcusVinicius1@users.noreply.github.com>
2024-04-09 09:53:20 -0300

    Add files via upload

---
+++ Portifolio/assets/Script/Banco.js
@@ -0,0 +1,64 @@
+const ListaProjetos = [
+    {
+        Img: 'https://img.freepik.com/vetores-gratis/interface-do-aplicativo-music-player_23-2148532741.jpg?size=626&ext=jpg',
+        Nome: 'Ui card',
+        LinkGitHub: 'https://www.fronteditor.dev/',
+        LinkStart: 'https://www.youtube.com/'
+    },
+    {
+        Img: 'https://img.freepik.com/vetores-gratis/aplicativo-de-reserva-de-viagens_52683-42107.jpg?size=626&ext=jpg',
+        Nome: 'Ui card',
+        LinkGitHub: 'https://www.fronteditor.dev/',
+        LinkStart: 'https://www.youtube.com/'
+    },
+
+    {
+        Img: 'https://img.freepik.com/vetores-gratis/modelo-de-aplicativo-para-gerenciamento-de-casa-inteligente_23-2148636702.jpg?size=626&ext=jpg',
+        Nome: 'Ui card',
+        LinkGitHub: 'https://www.fronteditor.dev/',
+        LinkStart: 'https://www.youtube.com/'
+    },
+    {
+        Img: 'https://img.freepik.com/vetores-gratis/aplicativo-para-aprender-idiomas_23-2148624202.jpg?size=626&ext=jpg',
+        Nome: 'Ui card',
+        LinkGitHub: 'https://www.fronteditor.dev/',
+        LinkStart: 'https://www.youtube.com/'
+    },
+    {
+        Img: 'https://img.freepik.com/vetores-gratis/modelo-de-design-movel-de-padrao-geometrico-de-design-plano_23-2149939691.jpg?size=626&ext=jpg&ga=GA1.1.35765491.1711546366&semt=ais',
+        Nome: 'Ui card',
+        LinkGitHub: 'https://www.fronteditor.dev/',
+        LinkStart: 'https://www.youtube.com/'
+    },
+    {
+        Img: 'https://img.freepik.com/psd-premium/design-de-interface-ui-e-ux-para-smartphone_23-2150395031.jpg?size=626&ext=jpg&ga=GA1.1.35765491.1711546366&semt=ais',
+        Nome: 'Ui card',
+        LinkGitHub: 'https://www.fronteditor.dev/',
+        LinkStart: 'https://www.youtube.com/'
+    },
+    {
+        Img: 'https://img.freepik.com/vetores-premium/gerenciamento-domestico-inteligente-em-tons-violeta_23-2148620923.jpg?size=626&ext=jpg&ga=GA1.1.35765491.1711546366&semt=ais',
+        Nome: 'Ui card',
+        LinkGitHub: 'https://www.fronteditor.dev/',
+        LinkStart: 'https://www.youtube.com/'
+    },
+    {
+        Img: 'https://img.freepik.com/vetores-premium/meu-medico-app-modelo-de-design-de-aplicativo-de-suporte-on-line-de-cuidados-de-saude_225543-13.jpg?size=626&ext=jpg&ga=GA1.1.35765491.1711546366&semt=ais',
+        Nome: 'Ui card',
+        LinkGitHub: 'https://www.fronteditor.dev/',
+        LinkStart: 'https://www.youtube.com/'
+    },
+
+    {
+        Img: 'https://img.freepik.com/vetores-gratis/modelo-de-interface-de-aplicativo-de-gerenciamento-de-tarefas_23-2148681557.jpg?size=626&ext=jpg&ga=GA1.1.35765491.1711546366&semt=ais',
+        Nome: 'Ui card',
+        LinkGitHub: 'https://www.fronteditor.dev/',
+        LinkStart: 'https://www.youtube.com/'
+    },
+    {
+        Img: 'https://img.freepik.com/vetores-gratis/modelo-de-aplicativo-de-coworking-de-design-plano_23-2150249518.jpg?size=626&ext=jpg&ga=GA1.1.35765491.1711546366&semt=ais',
+        Nome: 'Ui card',
+        LinkGitHub: 'https://www.fronteditor.dev/',
+        LinkStart: 'https://www.youtube.com/'
+    },
+]
